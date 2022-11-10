e11bbf6b601ec2cc7329c1c1aec3352441127354
Kelvin Ben <kevoben14@gmail.com>
2022-11-10 22:18:44 +0300

    Fetch list of books

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooks } from '../redux/books/books';
 import BookFeatures from './BookFeatures';
 import AddBook from './Form';
 
 export default function BooksContainer() {
   const books = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
   return (
     <>
       <div id="books-list">
-        {books.map((book) => (
-          <BookFeatures key={book.id} book={book} />
-        ))}
+        {books.map((book) => <BookFeatures key={book.item_id} book={book} />)}
       </div>
       <AddBook />
     </>
